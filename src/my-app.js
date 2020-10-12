7a4df8787a98b80e8ec21a8b369df72aa3037f21
suvirb <hitlin37@gmail.com>
2020-10-12 11:01:26 +0200

    Update my-app.js
    It works for now.

--- src/my-app.js
+++ src/my-app.js
@@ -1,25 +1,23 @@
-//import {
-//  inject
-//} from 'aurelia-framework';
+//import { inject } from "aurelia-framework";
 
-import * as RecordRTC from 'recordrtc';
-
-
+import * as RecordRTC from "recordrtc";
 
 var recorder;
 var theVideo;
+var stopButtonStatus = true;
 
 //@inject(Element)
 export class MyApp {
   //public clock: HTMLCanvasElement;
 
   //sourceObject = HTMLMediaElement.srcObject;
-
-
-
+  //var stopButtonStatus = new Boolean(true);
   //HTMLVideoElement theVideo;
 
-
+  constructor(element) {
+    this.element = element;
+    this.stopButtonStatus = true;
+  }
 
   /*   constructor() {
       //var recorder; // globally accessible
@@ -30,112 +28,153 @@ export class MyApp {
       };
     } */
 
-  attached() {
-
-    //const theVideo = this.element.querySelector('video');
+  afterAttach() {
+    //const theVideo = this.element.querySelector("video");
+    theVideo = this.theVideo; // same with the ref name
 
     //document.addEventListener('click', this.handleBodyClick);
 
     if (!navigator.getDisplayMedia && !navigator.mediaDevices.getDisplayMedia) {
-      var error = 'Your browser does NOT support the getDisplayMedia API.';
+      var error = "Your browser does NOT support the getDisplayMedia API.";
       //document.querySelector('h1').innerHTML = error;
       this.headerRef.innerHTML = error;
-      this.video.style.display = 'none';
+      this.video.style.display = "none";
       //document.getElementById('btn-start-recording').style.display = 'none';
-      this.btnStartRecording.style.display = 'none';
+      this.btnStartRecording.style.display = "none";
       //document.getElementById('btn-stop-recording').style.display = 'none';
-      this.btnStopRecording.style.display = 'none';
+      this.btnStopRecording.style.display = "none";
       throw new Error(error);
     }
-
-
   }
 
   addStreamStopListener(stream, callback) {
-    stream.addEventListener('ended', function () {
-      callback();
-      callback = function () {};
-    }, false);
-    stream.addEventListener('inactive', function () {
-      callback();
-      callback = function () {};
-    }, false);
+    stream.addEventListener(
+      "ended",
+      function () {
+        callback();
+        callback = function () {};
+      },
+      false
+    );
+    stream.addEventListener(
+      "inactive",
+      function () {
+        callback();
+        callback = function () {};
+      },
+      false
+    );
     stream.getTracks().forEach(function (track) {
-      track.addEventListener('ended', function () {
-        callback();
-        callback = function () {};
-      }, false);
-      track.addEventListener('inactive', function () {
-        callback();
-        callback = function () {};
-      }, false);
+      track.addEventListener(
+        "ended",
+        function () {
+          callback();
+          callback = function () {};
+        },
+        false
+      );
+      track.addEventListener(
+        "inactive",
+        function () {
+          callback();
+          callback = function () {};
+        },
+        false
+      );
     });
   }
 
   detached() {
-    document.removeEventListener('click', this.handleBodyClick);
+    document.removeEventListener("click", this.handleBodyClick);
   }
 
   invokeGetDisplayMedia(success, error) {
     var displaymediastreamconstraints = {
       video: {
-        displaySurface: 'monitor', // monitor, window, application, browser
+        displaySurface: "monitor", // monitor, window, application, browser
         logicalSurface: true,
-        cursor: 'always' // never, always, motion
-      }
+        cursor: "always", // never, always, motion
+      },
     };
-
 
     // above constraints are NOT supported YET
     // that's why overriding them
     displaymediastreamconstraints = {
-      video: true
+      video: true,
     };
 
     if (navigator.mediaDevices.getDisplayMedia) {
-      navigator.mediaDevices.getDisplayMedia(displaymediastreamconstraints).then(success).catch(error);
+      navigator.mediaDevices
+        .getDisplayMedia(displaymediastreamconstraints)
+        .then(success)
+        .catch(error);
     } else {
-      navigator.getDisplayMedia(displaymediastreamconstraints).then(success).catch(error);
+      navigator
+        .getDisplayMedia(displaymediastreamconstraints)
+        .then(success)
+        .catch(error);
     }
   }
 
-
   captureScreen(callback) {
-    this.invokeGetDisplayMedia(function (screen) {
-      /* this.addStreamStopListener(screen, function () {
+    this.invokeGetDisplayMedia(
+      function (screen) {
+        /* this.addStreamStopListener(screen, function () {
         //this.btnStopRecording.click;
       }); */
-      callback(screen);
-    }, function (error) {
-      console.error(error);
-      alert('Unable to capture your screen. Please check console logs.\n' + error);
-    });
+        callback(screen);
+      },
+      function (error) {
+        console.error(error);
+        alert(
+          "Unable to capture your screen. Please check console logs.\n" + error
+        );
+      }
+    );
   }
-
 
   startRecording = () => {
     console.log("clicked start");
-    this.disabled = true;
+    //this.disabled = true;
 
-    this.captureScreen(function (screen) {
+    this.btnStartRecording.disabled = true;
+
+    var localthis = this;
+
+    this.captureScreen((screen) => {
       //var video = document.theVideo;
       //var video = this.theVideo; // this should have worked
+      //const theVideo = this.element.querySelector('video');
       theVideo.srcObject = screen;
       //document.theVideo.HTMLMediaElement.srcObject = screen;
 
-      this.recorder = RecordRTC(screen, {
-        type: 'video'
+      recorder = RecordRTC(screen, {
+        type: "video",
       });
 
-      this.recorder.startRecording();
+      recorder.startRecording();
 
       // release screen on stopRecording
-      this.recorder.screen = screen;
+      recorder.screen = screen;
 
-      this.btnStopRecording.disabled = false;
+      //this.document.getElementById("btnStopRecording").stopButtonStatus = false;
+
+      //this.stopButtonStatus = false;
+      console.log("capture ...");
+      //this.btnStopRecording.stopButtonStatus = true;
+
+      localthis.stopButtonStatus = false;
+
+      //this.btnStopRecording.disabled = false;
+      //this.element.querySelector("#searchInput") = "";
+      //this.stopButtonStatusCChanged();
     });
   };
 
+  /*   get stopButtonStatusCChanged() {
+    console.log("stopButtonStatusCChanged");
+    return true;
+  } */
 
   /*   startRecording() {
      
@@ -143,22 +182,22 @@ export class MyApp {
 
   stopRecording = () => {
     console.log("clicked stop");
-    this.disabled = true;
-    this.recorder.stopRecording(this.stopRecordingCallback);
-  }
-
+    //this.disabled = true;
+    this.stopButtonStatus = true;
+    recorder.stopRecording(this.stopRecordingCallback);
+  };
 
   stopRecordingCallback() {
-    this.theVideo.src = this.theVideo.srcObject = null;
-    this.theVideo.src = URL.createObjectURL(this.recorder.getBlob());
+    theVideo.src = theVideo.srcObject = null;
+    theVideo.src = URL.createObjectURL(recorder.getBlob());
 
-    this.recorder.screen.stop();
-    this.recorder.destroy();
-    this.recorder = null;
+    recorder.screen.stop();
+    recorder.destroy();
+    recorder = null;
 
-    //document.getElementById('btn-start-recording').disabled = false;
-    this.btnStartRecording.disabled = false;
+    //document.getElementById("btn-start-recording").disabled = false;
+    document.getElementById("btnStartRecording").disabled = false;
+
+    //this.btnStartRecording.disabled = false;
   }
-
-
 }
